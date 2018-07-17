4d7088edcf6f51f7d385323a0e6f71288c555f41
nchicaiza <38292837+nchicaiza@users.noreply.github.com>
2018-07-16 23:52:35 -0500

    imagenes

--- Examen/project/src/usuario/usuario.data.ts
+++ Examen/project/src/usuario/usuario.data.ts
@@ -1,8 +1,8 @@
 export const UsuarioData = [
-    { "nombre_usuario" : "Jorge Carrillo", "urlFoto" : "https://www.dropbox.com/s/e5d9kgkhkkqj8rn/Captura%20de%20pantalla%202018-07-14%20a%20la%28s%29%2018.37.12.png?dl=0"},
-    { "nombre_usuario" : "Luis Carrillo", "urlFoto" : "https://www.dropbox.com/s/1mn0z3a1qjqucc9/Captura%20de%20pantalla%202018-07-14%20a%20la%28s%29%2019.14.48.png?dl=0"},
-    { "nombre_usuario" : "Olga Lopez", "urlFoto" : "https://www.dropbox.com/s/j5a15928f94e8xy/Captura%20de%20pantalla%202018-07-14%20a%20la%28s%29%2019.14.56.png?dl=0"},
-    { "nombre_usuario" : "Cristhian Lopez", "urlFoto" : "https://www.dropbox.com/s/rfohtgvou9eqytx/Captura%20de%20pantalla%202018-07-14%20a%20la%28s%29%2019.15.07.png?dl=0"},
-    { "nombre_usuario" : "Gloria Lopez", "urlFoto" : "https://www.dropbox.com/s/uj3gurdsl8org4t/Captura%20de%20pantalla%202018-07-14%20a%20la%28s%29%2019.15.28.png?dl=0"},
-    { "nombre_usuario" : "Andres Hinojoza", "urlFoto" : "https://www.dropbox.com/s/uj3gurdsl8org4t/Captura%20de%20pantalla%202018-07-14%20a%20la%28s%29%2019.15.28.png?dl=0"},
+    { "nombre_usuario" : "Nelson Chicaiza", "urlFoto" : "https://epnecuador-my.sharepoint.com/:i:/g/personal/nelson_chicaiza_epn_edu_ec/EW-lkz47K_NMvPxhszDAcmkBC64mvt0ng-Rk1lJS9UjXmA?e=6iYyxx"},
+    { "nombre_usuario" : "Fredi Chicaiza", "urlFoto" : "https://epnecuador-my.sharepoint.com/:i:/g/personal/nelson_chicaiza_epn_edu_ec/EcOBakmkocRJlNT0iJn-_t8BJ6nHdtpfSFYbRbLZrzQcLQ?e=7Z9sva"},
+    { "nombre_usuario" : "Janet Sevillano", "urlFoto" : "https://epnecuador-my.sharepoint.com/:i:/g/personal/nelson_chicaiza_epn_edu_ec/EZFaSrt9uylIv7pCiSJpqIkBptanjxQwsy0Os77hHbGgjA?e=iGMmAO"},
+    { "nombre_usuario" : "Olga Simbaña", "urlFoto" : "https://epnecuador-my.sharepoint.com/:i:/g/personal/nelson_chicaiza_epn_edu_ec/EZmJqXUyKtRHuu5aPDl-E_kBgzcAQOEAl57kdYcB6Mxecg?e=Dc50mg"},
+    { "nombre_usuario" : "Guliana Chicaiza", "urlFoto" : "https://epnecuador-my.sharepoint.com/:i:/g/personal/nelson_chicaiza_epn_edu_ec/Eftyw_aE4HNEvJz3q7ggqGABplgANIRbdbO-4a1JTKFwJQ?e=h46dR1"},
+    { "nombre_usuario" : "Arturo Torres", "urlFoto" : "https://epnecuador-my.sharepoint.com/:i:/g/personal/nelson_chicaiza_epn_edu_ec/Ebt1B0XYa_JAhoBhzVEjiBUBf2kbzQ9WwHYqyNWEivMOcA?e=l8ReR6"},
 ];
